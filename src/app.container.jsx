c1d58ba90b50d3f3ff50a14074bd54850ce55a89
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2020-10-04 19:18:38 +0800

    fixed routes

--- src/app.container.jsx
+++ src/app.container.jsx
@@ -56,7 +56,7 @@ const AppContainer = ({ logoutAction, currentUser }) => {
       </AppBar>
 
       <Switch>
-        <Route path="/:channel" component={HomePage} />
+        <Route path="/tveks/:channel" component={HomePage} />
       </Switch>
     </React.Fragment>
   );
